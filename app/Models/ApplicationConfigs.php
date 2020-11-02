716e19816db409babbd7375859360207172b8241
dilan wijerathne <dilan505@gmail.com>
2020-11-02 22:05:18 +0530

    jwt from pc

---
+++ app/Models/ApplicationConfigs.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ApplicationConfigs extends Model
+{
+
+    protected $table = 'status_codes';
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\DataController;
+use App\Http\Controllers\Bdo\ApplicantController;
+
+use App\Http\Controllers\Application;
+
+
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +23,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+
+
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'authenticate']);    // 'UserController@authenticate'
+Route::get('/open', [DataController::class, 'open']);   //   'DataController@open'
+
+Route::group(['/middleware' => ['jwt.verify']], function () {
+    /// '/user', 'UserController@getAuthenticatedUser'
+    //  Route::get('/closed',[DataController::class,'closed']);    // '/closed', 'DataController@closed'
 });
+
+
+
+Route::get('/closed', [DataController::class, 'closed'])->middleware('jwt.verify');
+Route::get('/user',  [UserController::class, 'getAuthenticatedUser'])->middleware('jwt.verify');
+Route::post('/applicantInitialSubmit',  [ApplicantController::class, 'ApplicantInitialSubmit'])->middleware('jwt.verify');
+
+
+
+
+Route::post('/status', [Application::class, 'applicant_status']);
+Route::post('/gointo', [Application::class, 'goin_to_open']);
+Route::post('/individualaccounttypes', [Application::class, 'individual_account_types']);
