581c88c5d68383bef14ee1ad9f925eabf660c648
dilan wijerathne <dilan505@gmail.com>
2021-02-08 21:22:57 +0530

    search sql my onboarding log

--- .history/app/Http/Controllers/Application_dash_20210208211936.php
+++ .history/app/Http/Controllers/Application_dash_20210208211936.php
@@ -16,7 +16,7 @@ class Application_dash extends Controller
 
         $models = DB::table('applicant')
             ->select('ref', 'title',  'full_name', 'nic', 'primary_mobile_number', 'created_at', 'signed')
-            // ->where('bdo', $request->bdo)
+            ->where('bdo', $request->bdo)
             ->where('nic', '%' . $request->search . '%')
             ->orderBy('created_at', 'desc')
             ->limit($request->end)->offset($request->start - 1)
