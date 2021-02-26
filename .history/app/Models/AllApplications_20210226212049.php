36b0f1df00e41a23946fbd79bcba0e2a347adc7b
dilan wijerathne <dilan505@gmail.com>
2021-02-26 21:21:02 +0530

    customer query

---
+++ .history/app/Models/AllApplications_20210226212049.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AllApplications extends Model
+{
+
+    protected $table = 'all_applicants_list';
+}
